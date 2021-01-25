edfc9616ed74522a70b1d60b4fb79d9d599ccad6
Ivan-4884 <iv.dushkin@gmail.com>
2021-01-25 10:26:56 +0300

    Fix

--- src/main/java/ru/job4j/oop/Fix.java
+++ src/main/java/ru/job4j/oop/Fix.java
@@ -2,7 +2,11 @@ package ru.job4j.oop;
 
 public class Fix {
     private String desc;
+    public Fix(String text) {
+        desc = text;
+    }
     public static void main(String[] args) {
-        Fix name = new Fix();
+        String issue = "test";
+        Fix name = new Fix(issue);
     }
 }

4c7e9b423412aca2c34de6e53e06136d9d519143
Ivan-4884 <iv.dushkin@gmail.com>
2021-02-10 14:58:25 +0300

    Cat

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,25 +1,31 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
     private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
 
     public void giveNick(String nick) {
         this.name = nick;
     }
 
     public void show() {
-        System.out.println(this.name);
+        System.out.print(this.name);
+        System.out.println(this.food);
     }
 
     public static void main(String[] args) {
         Cat gav = new Cat();
-        gav.giveNick("gav");
+        gav.giveNick("gav - ");
+        gav.eat("kotleta");
         gav.show();
-        System.out.println("food kotleta");
         Cat black = new Cat();
-        black.giveNick("black");
+        black.giveNick("black - ");
+        black.eat("fish");
         black.show();
-        System.out.println("food fish");
     }
 
     /*public String soung() {

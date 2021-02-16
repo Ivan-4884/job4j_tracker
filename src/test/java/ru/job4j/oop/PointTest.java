b8687f9f6724d68090d65b9eef16555b94f8bd14
Ivan-4884 <iv.dushkin@gmail.com>
2021-02-16 11:41:33 +0300

    4 Расстояние между точками в 3d #384414

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+    @Test
+    public void distance() {
+        Point a = new Point(0, 4);
+        Point b = new Point(4, 4);
+        double dist = a.distance(b);
+        assertThat(dist, closeTo(4, 0.001));
+    }
+
+    @Test
+    public void distance3d() {
+        Point a = new Point(0, 2, 4);
+        Point b = new Point(0, 3,5);
+        double dist = a.distance3d(b);
+        assertThat(dist, closeTo(1.414,0.001));
+    }
+}

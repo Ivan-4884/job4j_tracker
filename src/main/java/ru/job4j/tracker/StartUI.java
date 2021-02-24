b89a30ba2a8bd432e7a7eb7d80ae1fbbbc5830a1
Ivan-4884 <iv.dushkin@gmail.com>
2021-02-24 12:00:07 +0300

    7 to String #384433

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,9 +8,15 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item datatime = new Item();
+        Item datatimeone = new Item(2021);
+        Item datatimetwo = new Item(777, "test");
         LocalDateTime created = datatime.getCreated();
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        /*DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String createdformat = created.format(format);
-        System.out.println(createdformat);
+        System.out.println(createdformat);*/
+        System.out.println(datatime);
+        System.out.println(datatimeone);
+        System.out.println(datatimetwo);
+        System.out.println(created);
     }
 }

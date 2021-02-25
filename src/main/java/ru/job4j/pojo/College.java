e752d384b3c974d14519638506f235dc777d1f9c
Ivan-4884 <iv.dushkin@gmail.com>
2021-02-25 14:12:11 +0300

    2 Модель данных #384437

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,25 @@
+package ru.job4j.pojo;
+
+import java.time.LocalDate;
+
+public class College {
+    public static void main(String[] args) {
+        Student oneStudent = new Student();
+        oneStudent.setName("Ivan");
+        oneStudent.setSurname("Dushkin");
+        oneStudent.setPatronymic("Viktorovich");
+        oneStudent.setGroup("PM");
+        oneStudent.setData(LocalDate.of(2020, 12, 12));
+
+        System.out.printf(
+                "Student: %s %s %s." + System.lineSeparator() +
+                        "Group is: %s." + System.lineSeparator() +
+                        "Date is: %s.",
+                oneStudent.getName(),
+                oneStudent.getSurname(),
+                oneStudent.getPatronymic(),
+                oneStudent.getGroup(),
+                oneStudent.getData()
+        );
+    }
+}

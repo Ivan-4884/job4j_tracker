bdeb4565d680f03d96afbfa8a48bd0a4f1970b91
Ivan-4884 <iv.dushkin@gmail.com>
2021-09-12 14:40:51 +0300

    3.ci

--- src/main/java/ru/job4j/oop/Product.java
+++ src/main/java/ru/job4j/oop/Product.java
@@ -4,6 +4,7 @@ public final class Product {
     private String name;
     private int price;
 
+
     public Product(String name, int price) {
         this.name = name;
         this.price = price;

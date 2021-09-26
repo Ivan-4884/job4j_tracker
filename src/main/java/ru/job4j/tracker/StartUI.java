365bcf5f6f66a40bc289cd513f8b6c8401c2e385
Ivan-4884 <iv.dushkin@gmail.com>
2021-09-26 15:16:43 +0300

    2. Scanner и чтение числа из консоли. [#179518]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,22 +1,38 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.print("Select: ");
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
+                run = false;
+            }
+        }
 
+    }
+
+    private void showMenu() {
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
+    }
 
     public static void main(String[] args) {
-        Item datatime = new Item();
-        Item datatimeone = new Item(2021);
-        Item datatimetwo = new Item(777, "test");
-        LocalDateTime created = datatime.getCreated();
-        /*DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String createdformat = created.format(format);
-        System.out.println(createdformat);*/
-        System.out.println(datatime);
-        System.out.println(datatimeone);
-        System.out.println(datatimetwo);
-        System.out.println(created);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
